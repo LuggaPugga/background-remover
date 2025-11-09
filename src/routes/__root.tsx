6f88b035771e08533c0d824cdd07d60da520ea86
Luca <lucameopou@gmail.com>
2025-11-09 20:11:12 +0100

    chore: remove unused TanStack Devtools from project and update package dependencies

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,4 @@
-import { TanStackDevtools } from "@tanstack/react-devtools";
 import { createRootRoute, HeadContent, Scripts } from "@tanstack/react-router";
-import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import { ThemeProvider } from "@/components/theme-provider";
 import appCss from "../styles.css?url";
 
@@ -80,17 +78,6 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 				<ThemeProvider enableSystem attribute="class">
 					{children}
 				</ThemeProvider>
-				<TanStackDevtools
-					config={{
-						position: "bottom-right",
-					}}
-					plugins={[
-						{
-							name: "Tanstack Router",
-							render: <TanStackRouterDevtoolsPanel />,
-						},
-					]}
-				/>
 				<Scripts />
 			</body>
 		</html>
